640ab5912977ba64d6651052cf3f1d9d40b79aa5
Jim Kratus <32602483+jkrat@users.noreply.github.com>
2019-11-14 15:47:42 -0600

    attempt to fetch transactions not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import RevvedCalendar from './components/RevvedCalendar';
 
-const App = () => <RevvedCalendar />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      transactions: {},
+    };
+  }
+
+  componentWillMount() {
+    fetch('Api/Transactions')
+      .then(data => data.json())
+      .then(
+        result => {
+          console.log('connected');
+          this.setState({
+            transactions: result,
+          });
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  render() {
+    return <RevvedCalendar />;
+  }
+}
 
 export default App;
